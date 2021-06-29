e08610a2edf4aa24a17ebca0b4040c8244423c12
KimGH-kor <socoolq@naver.com>
2021-06-29 12:35:41 +0900

    7

--- HelloWoled2/Main.java
+++ HelloWoled2/Main.java
@@ -1,28 +1,31 @@
 package HelloWoled2;
 import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();//과목 수
-        String C[] = new String[A]; 
+        String A = sc.next();//값을 받아옴
+        int[] B = new int[123];// 아스키코드 소문자 97~122
+        int C = 1;
         
+        for(int i = 97; i < 123; i++) {
+        	B[i] = -1;
+        }
         
-        for(int i = 0; i < A; i++){
-        	String B = sc.next();
-            C[i] = B;
-            int result = 0;
-            int count = 0;
-            for(int k = 0; k < C[i].length(); k++){
-            if(C[i].charAt(k) == 'O'){
-                count++;
-                result += count;
-            }else{
-                count = 0;
-            }
-            }
-            System.out.println(result);   
+        for(int i = 0; i < A.length(); i++){//배열 각 자리에 값을 넣음 ->앞에 자리일수록 값에 +씩 늘어남
+        	char D = A.charAt(i);
+        	if(B[(int)D] == -1) {
+        	B[(int)D] += C;
+        	C++;
+        	}else {
+        		C++;
+        	}
+        	
+        }
+        for(int i =97; i < 123; i++) {
+        	System.out.print(B[i]+" ");
         }
         
         
-        }
+    }
 }
