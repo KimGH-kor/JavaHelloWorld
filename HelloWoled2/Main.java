90213e2701576e3325ffbdeb700898d6a45015e7
KimGH-kor <socoolq@naver.com>
2021-06-30 11:04:11 +0900

    8

--- HelloWoled2/Main.java
+++ HelloWoled2/Main.java
@@ -1,31 +1,21 @@
 package HelloWoled2;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String A = sc.next();//값을 받아옴
-        int[] B = new int[123];// 아스키코드 소문자 97~122
-        int C = 1;
-        
-        for(int i = 97; i < 123; i++) {
-        	B[i] = -1;
+		public static void main(String[] args){
+		    Scanner a = new Scanner(System.in);
+		    String sc = a.nextLine().trim();
+		    int answer = 0;
+		    
+            if(sc.length() != 0){
+		    for(int i = 0; i < sc.length(); i++){
+		        if(sc.charAt(i)==' '){
+		            answer += 1;
+		        }
+		    }
+            answer++;
+		    
+		}
+            System.out.println(answer);
         }
-        
-        for(int i = 0; i < A.length(); i++){//배열 각 자리에 값을 넣음 ->앞에 자리일수록 값에 +씩 늘어남
-        	char D = A.charAt(i);
-        	if(B[(int)D] == -1) {
-        	B[(int)D] += C;
-        	C++;
-        	}else {
-        		C++;
-        	}
-        	
-        }
-        for(int i =97; i < 123; i++) {
-        	System.out.print(B[i]+" ");
-        }
-        
-        
-    }
-}
+	}
